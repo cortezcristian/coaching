de6343f7573b9f78f6e514fc51e49cf57c221b1b
Cristian Cortez <cortez.cristian@gmail.com>
2017-11-13 22:37:55 -0300

    Basics

---
+++ torun.sh
@@ -0,0 +1,27 @@
+yo reveal:slide "Confianza e intimidad con el cliente"
+yo reveal:slide "Confianza Capacidad"
+yo reveal:slide "El ser y el tiempo"
+yo reveal:slide "Trasmitir tranquilidad"
+yo reveal:slide "Cuenta de Banco Emocional" # yo reveal:slide "Post mortem"
+yo reveal:slide "The speed of trust" # https://www.youtube.com/watch?v=lvIEfNyZ8B0
+# trust is finantial
+# sos muy confiado => speed
+# no enseñes todo lo que sabes
+# cuento del gato y el tigre
+# el gato lo inventó como inventó todos los otros trucos cuando se vio en necesidad
+# por que seguía sintiendo que estaba en lo correcto?
+# todo cuesta menos, high trust, less friction, more colaboration, speed, lower costs
+# juicio => a confianza es una emocion =>
+# elegi una persona que ya confiaba en mi
+# proyectan futuras performances
+yo reveal:slide "Confianza en el cliente"
+yo reveal:slide "Estrategias para desarrollar la confianza"
+yo reveal:slide "Diferentes niveles de competencia"
+yo reveal:slide "Sesión con Matias"
+yo reveal:slide "Objetivo Cuantificables y Medibles"
+# yo reveal:slide "Contrastando el Estado Actual"
+# Realidad comprar un libro seguridad, que obstaculos ves
+yo reveal:slide "Contrastando el Estado Actual con el Esperado"
+yo reveal:slide "Diviendo el problema en pequeñas partes"
+yo reveal:slide "The Talk"
+yo reveal:slide "Follow Up"
